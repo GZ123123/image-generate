bb56fda4999befad06a3ed47dd61ad994eebc9f0
Sang Nguyen <34164266+GZ123123@users.noreply.github.com>
2023-10-27 14:35:48 +0700

    update app title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
+        <title>{process.env.NEXT_PUBLIC_TITLE}</title>
+        <meta property="og:title" content={process.env.NEXT_PUBLIC_TITLE} key="title" />
       </Head>
       <body>
         <Main />
