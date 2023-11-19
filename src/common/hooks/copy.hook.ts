3dbbf4081b876e0cd522545909b97b15accf189d
congnv3030 <congnv3030@gmail.com>
2023-11-19 14:52:48 +0700

    update: add fallback copy to clipboard

--- src/common/hooks/copy.hook.ts
+++ src/common/hooks/copy.hook.ts
@@ -10,8 +10,35 @@ export const useCopyToClipboard = (text: string) => {
     [isDark]
   );
 
-  const copy = useCallback(() => {
-    navigator.clipboard.writeText(text.trim());
+  const fallbackCopyTextToClipboard = (text: string) => {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+      if (document.execCommand('copy')) {
+        alert("success");
+      }
+    } catch (err) {
+    } finally {
+      document.body.removeChild(textArea);
+    }
+  }
+
+  const copy = useCallback(async () => {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text.trim());
+      return;
+    }
+
+    await navigator.clipboard.writeText(text.trim());
     alert("success");
   }, [text, alert]);
 
